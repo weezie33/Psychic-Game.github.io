1837719e6b92625966a7bf2ca8adad29fc1ec2c6
Wezzie33 <corey.webeck@gmail.com>
2019-03-14 22:09:29 -0400

    Psychic-Game-github.io

--- javascript/game.js
+++ javascript/game.js
@@ -1,165 +1,68 @@
+//References for this html/js come from GWARL201901FSF3-FT 23-RPS-Coded Solved rps-7.html. This source is from the GW Coding Bootcamp, as well as terminal references from w3school.com, and Meri Engel, "merihelp.net:JavaScript part two of the word guessing game.mp4", https://www.youtube.com/watch?v=6hEjUIRuGSU&t=9s; Retrieved March 15, 2019.
 
-    var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Alert the player the basic instructions of the game.
+  alert("Welcome, pick a letter between 'a' and 'z' but choose wisely as you only have nine tries.");
 
+//Variables
+  var wins = 0;
+  var losses = 0;
+  var userGuess = 10;
+  var previousGuesses = [""];
+  var computerChoice = computerChoice;
+  var letters = "abcdefghijklmnopqrstuvwxyz";
 
-    var directions = 0;
+//The computer options are depicted by var letters and translated into an array and numeric strucure of 0 to 25, given that there are 26 letters in the English alphabet.
+  computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice);
 
-    var computerChoice = 0;
-    var wins = 0;
-    var losses = 0;
-    var guessesRemaining = 9;
-    var choicesSoFar = [];
+//The computers picks a random number from the array and logs it to the console. 
+  function computerGuess() {
+    letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice);
+  }
 
+//Once the player acknowledges the alert the next letter key pushed will run the function as an "event".
+  document.onkeyup = function(event) {
+    var usersGuess = event.key;
 
-    document.onkeyup = function(event) {
-      var userGuess = event.key;
-      var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-      var computerOption = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    };
-
-    if (computerOption.indexOf(userGuess) > -1) {
-
-    if (userGuess === computerGuess) {
+//If the user chooses the same letter as the computer the user scores a point (++ — Increment numbers), wins++; there are a total of nine guesses per round.
+  if(usersGuess == computerChoice) {
     wins++;
-    guessesRemaining = 9;
-    guessChoice = [];
+    userGuess = 9;
+    previousGuesses = [""];
+  }
+
+//If the user chooses a different letter than the one choosen by computer (!== — Not equal value or not equal type) 1 guess will be removed from the userGuess, e.g., 9 -1 = 8 (-- Decrement numbers).
+  computerGuess();
+    if(usersGuess !== computerChoice) {
+      userGuess--;
+  }
+
+//When the userGuess reach zero one point (++) is added to the computer and the guesses are reset to 9.
+  if(userGuess == 0) {
+    losses++;
+    previousGuesses = [""]
+    userGuess = 9;
+  }
+
+// If a non alphabetical key is hit it will subtract one userGuess.
+  if(previousGuesses.indexOf(usersGuess) >= 0) {
+
+  } else {
+//The push() method adds new items to the end of an array, and returns the new length.
+  previousGuesses.push(usersGuess);
+
+// The innerHTML property can be used to get or change any HTML element, including <html> and <body>.
+  document.getElementById("wins").innerHTML = "Wins: " + wins;
+    console.log(wins);
+  
+  document.getElementById("losses").innerHTML =  "Losses: " + losses;
+    console.log(losses);
+  
+  document.getElementById("userGuess").innerHTML = "Guesses Remaining: " + userGuess;
+    console.log(userGuess);
+  
+  document.getElementById("previousGuesses").innerHTML = "Your Guesses So Far: " + previousGuesses;
+    console.log(previousGuesses);
     }
-
-    if (userGuess != computerGuess) {
-    guessesRemaining --;
-    guessChoice.push(userGuess);
-    }
-
-    if (guessesRemaining === 0) {
-
-    guessesRemaining = 9;
-    losses ++;
-    guessChoice = [];
-
-
-    }
-
-    var html = 
-    "<h1> The Psychic Game </h1>" +
-    "<p>Guess what letter I'm thinking of!</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses Left: " + guessesRemaining + "</p>" +
-    "<p>Your Guesses so far: " + guessChoice.join(", ") + "</p>";
-
-    document.querySelector("#game").innerHTML = html;
-
-
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if (options.indexOf(userGuess) > -1) {
-
-// 				if (userGuess === computerGuess) {
-// 					wins++;
-// 					numGuesses = 9;
-// 					guessChoices = [];
-// 				}
-
-// 				if (userGuess != computerGuess) {
-// 					numGuesses --;
-// 					guessChoices.push(userGuess);
-// 				}
-
-// 				if (numGuesses === 0) {
-
-// 				numGuesses = 9;
-// 				losses ++;
-// 				guessChoices = [];
-
-				
-// 			}
-
-
-
-
-
-//     // Create variables that hold references to the places in the HTML where we want to display things.
-//     var directions = document.getElementById("directions");
-//     var userChoice = document.getElementById("userchoice");
-//     var computerChoice = document.getElementById("computerchoice");
-//     var wins = document.getElementById("wins");
-//     var losses = document.getElementById("losses");
-//     var guessesSoFar = document.getElementById("guessesremaining");
-//     var guessesSoFar = document.getElementById("guessesthusfar");
-
-		
-// 		 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-// 		 var wins = 0;
-// 		 var losses = 0;
-// 		 var numGuesses = 9;
-// 		 var guessChoices = [];
-
-// 		 document.onkeyup = function(event) {
-
-// 		 	var userGuess = event.key;
-
-// 		 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-// 		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-		 	
-
-// 			if (options.indexOf(userGuess) > -1) {
-
-// 				if (userGuess === computerGuess) {
-// 					wins++;
-// 					numGuesses = 9;
-// 					guessChoices = [];
-// 				}
-
-// 				if (userGuess != computerGuess) {
-// 					numGuesses --;
-// 					guessChoices.push(userGuess);
-// 				}
-
-// 				if (numGuesses === 0) {
-
-// 				numGuesses = 9;
-// 				losses ++;
-// 				guessChoices = [];
-
-				
-// 			}
-
-// 			var html = 
-// 			"<h1> The Psychic Game </h1>" +
-// 			"<p>Guess what letter I'm thinking of!</p>" +
-// 			"<p>Wins: " + wins + "</p>" +
-// 			"<p>Losses: " + losses + "</p>" +
-// 			"<p>Guesses Left: " + numGuesses + "</p>" +
-// 			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
-
-// 			document.querySelector("#game").innerHTML = html;
-
-			
-// 			}
-// 		};
-
-// 	</script>
-
-// </body>
-// </html>
+  }
